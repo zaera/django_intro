0749c82655415b73f7130c749eca9d017afc6075
alex <zaera.alex@gmail.com>
2021-05-27 18:03:43 +0300

    add flake8 and travis ci

--- currency_app/views.py
+++ currency_app/views.py
@@ -1,35 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse("""
-                        <html>
-                        <style>
-                            .center {
-                            padding: 25% 0;
-                            text-align: center;
-                            }
-                        </style>
-                        <div class=center><h3>Hello world!</h3>
-                        <p>
-                        <h1><label id='loading'></label><h1>
-                        </div>
-                        </html>
-                        <script>
-                            var loading = (function() {
-                              var h = ['|', '/', '-', ")"];
-                              var i = 0;
-                              return setInterval(() => {
-                                i = (i > 3) ? 0 : i;
-                                document.getElementById('loading').innerHTML=h[i];
-                                i++;
-                              }, 50);
-                            })();
-                        </script>
-                        """)
-
-def index_page(request):
     return HttpResponse("""
                             <html>
                             <style>
@@ -37,6 +12,34 @@ def index_page(request):
                                 padding: 25% 0;
                                 text-align: center;
                                 }
+                            </style>
+                            <div class=center><h3>Hello world!</h3>
+                            <p>
+                            <h1><label id='loading'></label><h1>
+                            </div>
+                            </html>
+                            <script>
+                                var loading = (function() {
+                                  var h = ['|', '/', '-', ")"];
+                                  var i = 0;
+                                  return setInterval(() => {
+                                    i = (i > 3) ? 0 : i;
+                                    document.getElementById('loading').innerHTML=h[i];
+                                    i++;
+                                  }, 50);
+                                })();
+                            </script>
+                        """)
+
+
+def index_page(request):
+    return HttpResponse("""
+                            <html>
+                            <style>
+                            .center {
+                            padding: 25% 0;
+                            text-align: center;
+                            }
                             </style>
                             <body>
                             <div class=center>
