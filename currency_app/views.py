3a7f7ba6024be1460a05361910b92babf294efdb
alex <zaera.alex@gmail.com>
2021-05-27 18:16:31 +0300

    fix flake8 errors

--- currency_app/views.py
+++ currency_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
 
 
@@ -6,48 +6,47 @@ from django.http import HttpResponse
 
 def hello_world(request):
     return HttpResponse("""
-                            <html>
-                            <style>
-                                .center {
-                                padding: 25% 0;
-                                text-align: center;
-                                }
-                            </style>
-                            <div class=center><h3>Hello world!</h3>
-                            <p>
-                            <h1><label id='loading'></label><h1>
-                            </div>
-                            </html>
-                            <script>
-                                var loading = (function() {
-                                  var h = ['|', '/', '-', ")"];
-                                  var i = 0;
-                                  return setInterval(() => {
-                                    i = (i > 3) ? 0 : i;
-                                    document.getElementById('loading').innerHTML=h[i];
-                                    i++;
-                                  }, 50);
-                                })();
-                                cosnole.log(1)
-                            </script>
+                        <html>
+                        <style>
+                            .center {
+                            padding: 25% 0;
+                            text-align: center;
+                            }
+                        </style>
+                        <div class=center><h3>Hello world!</h3>
+                        <p>
+                        <h1><label id='loading'></label><h1>
+                        </div>
+                        </html>
+                        <script>
+                            var loading = (function() {
+                              var h = ['|', '/', '-', ")"];
+                              var i = 0;
+                              return setInterval(() => {
+                                i = (i > 3) ? 0 : i;
+                                document.getElementById('loading').innerHTML=h[i];
+                                i++;
+                              }, 50);
+                            })();
+                            cosnole.log(1)
+                        </script>
                         """)
 
 
 def index_page(request):
     return HttpResponse("""
-                            <html>
-                            <style>
-                            .center {
-                            padding: 25% 0;
-                            text-align: center;
-                            }
-                            </style>
-                            <body>
-                            <div class=center>
-                            <h3>
-                            My first page on django. <p><a href="/hello_world">Link to hello world</a>
-                            </h3>
-                            </div>
-                            </body>
-                            </html>                                                                                                                   
-                        """)
+                        <html>
+                        <style>
+                        .center {
+                        padding: 25% 0;
+                        text-align: center;
+                        }
+                        </style>
+                        <body>
+                        <div class=center>
+                        <h3>
+                        My first page on django. <p><a href="/hello_world">Link to hello world</a>
+                        </h3>
+                        </div>
+                        </body>
+                        </html>""")
