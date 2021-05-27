9efe29d23e005aab725251dcdca93f3d65ec4370
alex <zaera.alex@gmail.com>
2021-05-27 18:42:26 +0300

    create model ContactUs

--- currency_app/models.py
+++ currency_app/models.py
@@ -1,3 +1,7 @@
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+class ContactUs(models.Model):
+    email_from = models.EmailField(max_length=254)
+    subject = models.CharField(max_length=255)
+    message = models.CharField(max_length=500)
