7eb8950f30bc3e58258f8d12c1288cdb46c054d1
alex <zaera.alex@gmail.com>
2021-06-01 00:31:59 +0300

    gen_data command created. Rate objects created. ContactUs filled up with a faker

---
+++ currency_app/management/commands/gen_data.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand
+from currency_app.models import Rate
+from currency_app.models import ContactUs
+from faker import Faker
+import random
+from random import randrange
+
+
+class Command(BaseCommand):
+    # help = 'My awesome generating data tool'
+    def handle(self, *args, **options):
+        fake = Faker()
+        source_list = [
+            'monobank',
+            'privatbank',
+            'vkurse',
+        ]
+        for i in range(100):
+            Rate.objects.create(
+                moneytype='USD',
+                sale=round(random.uniform(20.00, 29.99), 2),
+                buy=round(random.uniform(20.00, 29.99), 2),
+                source=source_list[randrange(3)],
+            )
+        for i in range(100):
+            ContactUs.objects.create(
+                 email_from=fake.free_email(),
+                 subject=fake.text(max_nb_chars=50),
+                 message=fake.text(max_nb_chars=200),
+             )
--- currency_app/models.py
+++ currency_app/models.py
@@ -2,7 +2,25 @@ from django.db import models
 # Create your models here.
 
 
+class Rate(models.Model):
+    moneytype = models.CharField(max_length=5)
+    sale = models.DecimalField(max_digits=5, decimal_places=2)
+    buy = models.DecimalField(max_digits=5, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    source = models.CharField(max_length=64)
+
+
 class ContactUs(models.Model):
     email_from = models.EmailField(max_length=254)
     subject = models.CharField(max_length=255)
     message = models.CharField(max_length=500)
+
+# @classmethod
+#     def create_random_rate(cls):
+#         import random
+#         cls.objects.create(
+#             currency=random.choice([1, 2]),
+#             buy=random.randint(20, 30),
+#             sale=random.randint(20, 30),
+#             source=random.choice([1, 2]),
+#         )
