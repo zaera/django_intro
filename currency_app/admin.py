2be39c18e2e957f05f983e550b064b1d324fd47b
Alex <zaera.alex@gmail.com>
2021-06-15 19:12:05 +0300

    admin > filters are added

--- currency_app/admin.py
+++ currency_app/admin.py
@@ -5,6 +5,7 @@ from .models import Rate, Bank, ContactUs
 
 class RateAdmin(admin.ModelAdmin):
     list_display = ('moneytype', 'sale', 'buy', 'created', 'source')
+    list_filter = ('moneytype', 'created', 'source')
 
 
 admin.site.register(Rate, RateAdmin)
@@ -12,6 +13,7 @@ admin.site.register(Rate, RateAdmin)
 
 class BankAdmin(admin.ModelAdmin):
     list_display = ('name', 'url')
+    list_filter = ('name',)
 
 
 admin.site.register(Bank, BankAdmin)
@@ -19,6 +21,7 @@ admin.site.register(Bank, BankAdmin)
 
 class ContactUsAdmin(admin.ModelAdmin):
     list_display = ('email_from', 'subject', 'message', 'created')
+    list_filter = ('created',)
 
 
 admin.site.register(ContactUs, ContactUsAdmin)
