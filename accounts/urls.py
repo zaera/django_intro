92fdd3bbe4e879346ac524bc19fc594e6bb0d86d
Alex <zaera.alex@gmail.com>
2021-07-23 15:03:10 +0300

    choices added, changepass changed to views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,10 @@
-from django.conf.urls import url
 from django.urls import path
-from accounts import views
-from accounts.views import Profile
+from accounts.views import Profile, ChangePassword
 
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('profile/', Profile.as_view(), name='profile'),
-    url(r'^password/$', views.changepassword, name='change-password'),
+    path('password/', ChangePassword.as_view(), name='change-password'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,25 +1,19 @@
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
 from accounts.models import User
-from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib.auth import update_session_auth_hash
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 
 
-@login_required(login_url='/')
-def changepassword(request):
-    if request.method == 'POST':
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request, user)  # Important!
-            return redirect('/')
-    else:
-        form = PasswordChangeForm(request.user)
-    return render(request, 'change_password.html', {
-        'form': form,
-    })
+class ChangePassword(PasswordChangeView):
+    queryset = User.objects.all()
+    template_name = 'change_password.html'
+    success_url = reverse_lazy('index')
+    fields = (
+        'password',
+    )
+
+    def get_object(self, queryset=None):
+        return self.request.user
 
 
 class Profile(UpdateView):
